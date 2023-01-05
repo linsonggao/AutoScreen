11d4f041ae2f6361289fc9042be7ffa629d0ff6a
linsonggao <371925293@qq.com>
2023-01-05 14:55:30 +0800

    7.6

--- config/automake.php
+++ config/automake.php
@@ -3,7 +3,8 @@ return [
   //默认不筛选的上传值
   'default' => -1,
   //默认不做模糊匹配的字符串(varchar)字段
-  'string_equal' => [],
+  'string_equal' => ['rctscp_check_tp'],
+  'yuhuan_spcl_dses_clrtl_cncr_list_string_equal' => ['rctscp_check_tp'],
   //表多个模糊匹配字段配置,提交-暂时不支持多个值
   'search_key' => ['search_key'],
   //表多个模糊匹配字段配置,表字段
@@ -12,7 +13,8 @@ return [
   'intestine_patients_enums_arr' => [
     'is_sign' => [0 => '未签约', 1 => '已签约'],
     'follow_up_status' => [0 => '未随访', 1 => '已随访', 2 => '超时未随访'],
-    'gender' => [0 => '保密', 1 => '男', 2 => '女']
+    'gender' => [0 => '保密', 1 => '男', 2 => '女'],
+    'is_questionnaire_scn_flg' => [1 => '普筛', 0 => '机会性筛查']
   ],
   //字段说明枚举.需要不同的表字段独立//表名_enums_arr
   'questionnaires_logs_enums_arr' => [
@@ -32,18 +34,25 @@ return [
     'operate_after_status' => [0 => '未复诊', 1 => '已复诊'],
     'gender' => [0 => '保密', 1 => '男', 2 => '女']
   ],
-  //某表需要判断between的值
-  'intestine_patients_between_arr' => ['year_bth', 'operate_at'],
-  //某表需要判断between的值
+  //某表需要判断between的值，表名_between_arr
+  'intestine_patients_between_arr' => ['year_bth', 'operate_at', 'age', 'sms_msg_num', 'not_trtmt_sms_msg_num', 'wait_oprt_sms_msg_num'],
+  //某表需要判断between的值，表名_between_arr
   'questionnaires_logs_between_arr' => ['year_bth'],
-  //某表需要判断between的值
-  'intestine_patients_diag_logs_between_arr' => ['year_bth'],
-  //某表需要判断between的值
-  'intestine_patients_cure_logs_between_arr' => ['year_bth'],
+  //某表需要判断between的值，表名_between_arr
+  'intestine_patients_diag_logs_between_arr' => ['year_bth', 'age'],
+  //某表需要判断between的值，表名_between_arr
+  'intestine_patients_cure_logs_between_arr' => ['year_bth', 'age'],
   //需要判断大于的值
-  'intestine_patients_gt_arr' => [],
+  'intestine_patients_gt_arr' => ['age', 'year_bth', 'sms_msg_num', 'not_trtmt_sms_msg_num', 'wait_oprt_sms_msg_num'],
   //需要判断小于的值
-  'intestine_patients_lt_arr' => [],
+  'intestine_patients_lt_arr' => ['age', 'year_bth'],
   //二位数组需要多重判断，表名_in_multi
-  'yuhuan_spcl_dses_clrtl_cncr_list_in_multi' => ['age'], //需要提交参数为 age[0][0],age[0][1]
+  'intestine_patients_in_multi' => ['age'],
+  'yuhuan_spcl_dses_clrtl_cncr_list_in_multi' => ['age'], //需要提交参数为 age[0][0],age[0][1]或者age[0][0] = 1,100
+  //某表需要判断between的值，表名_between_arr
+  'yuhuan_spcl_dses_clrtl_cncr_list_between_arr' => ['age'], //
+  'yuhuan_spcl_dses_clrtl_cncr_list_gt_arr' => ['age'],
+  'patients_between_arr' => ['age'],
+  'patients_between_gt_arr' => ['age'],
+  'patients_in_multi' => ['age'],
 ];
