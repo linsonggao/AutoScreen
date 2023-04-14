20f5b75f58d2a46f805088d93a86ffea57f1a67f
linsonggao <371925293@qq.com>
2023-04-14 09:18:54 +0800

    验证包优化

--- src/Support/MakeValidateRequest.php
+++ src/Support/MakeValidateRequest.php
@@ -16,6 +16,11 @@ class MakeValidateRequest extends BaseRequest
     public function rules()
     {
         $nowActionKey = Route::currentRouteAction();
+        if ($ruleConfig = Cache::get($nowActionKey . 'rule')) {
+            return $ruleConfig;
+        }
+        list($ruleConfig, $attrConfig) = $this->makeValidateCache($nowActionKey);
+
         $ruleConfig = Cache::get($nowActionKey . 'rule') ?? [];
 
         return $ruleConfig;
@@ -30,7 +35,30 @@ class MakeValidateRequest extends BaseRequest
     {
         $nowActionKey = Route::currentRouteAction();
         $attrConfig = Cache::get($nowActionKey . 'attr') ?? [];
+        if ($attrConfig = Cache::get($nowActionKey . 'attr')) {
+            return $attrConfig;
+        }
+        list($ruleConfig, $attrConfig) = $this->makeValidateCache($nowActionKey);
+        $attrConfig = Cache::get($nowActionKey . 'attr') ?? [];
 
         return $attrConfig;
+    }
+
+    //更新路由验证的缓存
+    public function makeValidateCache($nowActionKey)
+    {
+        $ruleConfigs = config('makeValidate');
+        $ruleConfig = [];
+        $attrConfig = [];
+        foreach ($ruleConfigs as $param => $rule) {
+            if (in_array($nowActionKey, $rule[0])) {
+                $ruleConfig[$param] = $rule[1];
+                $attrConfig[$param] = $rule[2];
+            }
+        }
+        Cache::set($nowActionKey . 'rule', $ruleConfig);
+        Cache::set($nowActionKey . 'attr', $attrConfig);
+
+        return [$ruleConfig, $attrConfig];
     }
 }
