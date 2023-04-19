1565a80923aac3ed747be1cca41e710ec49a3747
linsonggao <371925293@qq.com>
2023-04-19 17:37:58 +0800

    自动验证完善

--- config/makeValidate.php
+++ config/makeValidate.php
@@ -1,39 +1,15 @@
 <?php
+use App\Http\Controllers\TestController;
 
 return [
-    'name'         => [
-        [\App\Http\Controllers\TestController::class . '@index'],
+    'name'    => [
+        [TestController::class . '@index'],
         ['bail', 'required', 'string'],
         '用户姓名',
     ],
-    'gender'       => [
-        [\App\Http\Controllers\TestController::class . '@index'],
-        ['bail', 'required', 'int'],
-        '用户性别',
-    ],
-    'card_no'      => [
-        [\App\Http\Controllers\TestController::class . '@index'],
-        ['bail', 'required', 'string'],
+    'card_no' => [
+        [TestController::class . '@index'],
+        ['bail', 'required', new Lsg\AutoScreen\Rules\IdCardRule],
         '身份证号',
-    ],
-    'address'      => [
-        [\App\Http\Controllers\TestController::class . '@index'],
-        ['bail', 'required', 'string'],
-        '患者地址',
-    ],
-    'village_code' => [
-        [\App\Http\Controllers\TestController::class . '@index'],
-        ['bail', 'required', 'string'],
-        '村code',
-    ],
-    'town_code'    => [
-        [\App\Http\Controllers\TestController::class . '@index'],
-        ['bail', 'required', 'string'],
-        '镇code',
-    ],
-    'is_risk_cro'  => [
-        [\App\Http\Controllers\TestController::class . '@index'],
-        ['bail', 'required', 'string'],
-        '是否添加到风险人群',
     ],
 ];
