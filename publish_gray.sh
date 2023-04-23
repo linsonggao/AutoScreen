af5376a7d98543dd06e1b0d7456297b23504f3b2
linsonggao <371925293@qq.com>
2023-04-23 11:58:08 +0800

    灰度

--- publish_gray.sh
+++ publish_gray.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 #灰度标签k8s-5498-yuhuan-v
-#defaultKey="k8s-5498-yuhuan-v"
-defaultKey="test-k8s-8888-yuhuan-v"
+defaultKey="k8s-5498-yuhuan-v"
 
 read -p "
 ------------------------------
 输入基础标签,回车使用默认值或者输入
 [默认值:$defaultKey]:
 ------------------------------
-" 
+"  devKey
 if [ ! -n "$devKey" ];then
     devKey=$defaultKey
 fi
+
+newVersion=$(git tag --sort=committerdate |grep "$defaultKey*"|tail -1)
 version=$(git tag --sort=committerdate |grep "$defaultKey*"|tail -1|awk '{split($1,arr,"-v"); print arr[2]}')
 
 #把.去掉
 version=${version//./}
 version=$(($version+1)) 
 
-
 finialVersion=${version:0:1}.${version:1:1}.${version:2:1}${version:3:1}
+
 read -p "
 ------------------------------
 输入提交备注,回车使用默认值或者输入
@@ -27,24 +28,17 @@ read -p "
 [默认值:自动提交-测试]:"  desc
 if [ ! -n "$desc" ];then
     desc="自动提交-测试"
-    else
-    desc="自动提交-"$desc
 fi
 
 git pull
 
 echo "拉取代码~"
 
-git add .
-
-git commit -m "$desc"
-
-echo "提交了代码~"
-
 read -p "
 ------------------------------
 确认发布最终版本是否正确
 回车使用默认值或者输入末尾版本号
+最新版本为$newVersion
 ------------------------------
 [默认值:$devKey$finialVersion]:"  VfinialVersion
 if [ ! -n "$VfinialVersion" ];then
